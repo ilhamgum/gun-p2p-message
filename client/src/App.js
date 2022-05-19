7d0196425d224b74fc5eb155e1d75f0f7cc0f2ee
ilhamgum <ilham.gumilang29@gmail.com>
2022-05-19 08:56:06 +0700

    trying gun authentication

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,130 @@
 import "./App.css";
-import Gun from "gun/gun";
+import GUN from "gun/gun";
+import SEA from "gun/sea";
+import React from "react";
+import { useState } from "react";
 
 export default function App() {
-  const gun = Gun({
+  // initialize gun
+  const gun = GUN({
     peers: ["http://localhost:3030/gun"],
   });
-  const messages = gun.get("message");
+  // create object references
+  const user = gun.user();
 
-  messages.map().on((e) => {
-    console.log(e);
-  });
+  // states
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [key, setKey] = useState("");
+
+  // hooks
+  function createUser() {
+    user.create(username, password, (ack) => {
+      console.log(ack);
+    });
+  }
+
+  function authUser() {
+    user.auth(username, password, (ack) => {
+      console.log("logged in, public key:", ack.get);
+    });
+  }
+
+  async function authPair() {
+    const pair = await SEA.pair((ack) =>
+      console.log("pair cb |", JSON.stringify(ack))
+    );
+    user.auth(pair, (ack) => {
+      setKey(JSON.stringify(pair));
+      console.log("success", ack);
+    });
+  }
+
+  function authKeyPair() {
+    user.auth(key, (ack) => console.log("success", ack));
+  }
+
+  function logout() {
+    user.leave();
+    if (user._.sea) {
+      console.log("not logout", user._.sea);
+    } else {
+      console.log("logged out", user._.sea);
+    }
+  }
+
+  function check() {
+    gun.on("auth", (ack) => console.log(ack));
+    if (user.is) {
+      console.log("loggedin, key= ", JSON.stringify(user.is));
+    } else {
+      console.log("not logged in", user.is);
+    }
+  }
 
   return (
     <div className="App">
-      <header className="App-header">
-        <form>
-          <label htmlFor="fname">Username:</label>
-          <input type="text" id="fname" name="fname"></input>
-          <label htmlFor="pass">Password:</label>
-          <input type="password" id="pass" name="pass"></input>
-          <input type="submit" id="login" name="login"></input>
-          <input type="submit" id="register" name="register"></input>
-        </form>
-      </header>
+      <input
+        type="text"
+        onChange={(e) => setUsername(e.target.value)}
+        value={username}
+        placeholder="Enter username"
+      ></input>
+      <input
+        type="password"
+        onChange={(e) => setPassword(e.target.value)}
+        value={password}
+        placeholder="Enter password"
+      ></input>
+      <input
+        type="text"
+        onChange={(e) => setKey(e.target.value)}
+        value={key}
+        placeholder="Enter key"
+      ></input>
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        type="button"
+        onClick={authUser}
+      >
+        Login
+      </button>
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        type="button"
+        onClick={authKeyPair}
+      >
+        Login Key
+      </button>
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        type="button"
+        onClick={authPair}
+      >
+        Login Pair
+      </button>
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        type="button"
+        onClick={createUser}
+      >
+        Register
+      </button>
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        type="button"
+        onClick={logout}
+      >
+        Logout
+      </button>
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        type="button"
+        onClick={check}
+      >
+        Check
+      </button>
+      <p>Key:{key}</p>
     </div>
   );
 }
