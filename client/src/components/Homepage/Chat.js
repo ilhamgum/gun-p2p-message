1bc00e0831b9b70a5945c16d942541d1b4df0d28
ilhamgum <ilham.gumilang29@gmail.com>
2022-06-10 15:42:06 +0700

    send message

--- client/src/components/Homepage/Chat.js
+++ client/src/components/Homepage/Chat.js
@@ -1,13 +1,24 @@
 import React from "react";
 
-export default function Chat({pub, alias}) {
+export default function Chat({ pub, alias }) {
+  const [newMessage, setNewMessage] = React.useState("");
   return (
     <div>
       <p>alias: {alias} </p>
-      <p >pubkey: {pub} </p>
-      <div style={{width: "50%"}}></div>
-      <input placeholder="insert a messsage"/>
-      <button>Send</button>
+      <p>pubkey: {pub} </p>
+      {/* chats */}
+      <input
+        type="text"
+        placeholder="insert a messsage"
+        onChange={(e) => setNewMessage(e.target.value)}
+      />
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        type="button"
+        onClick={() => console.log("sent", newMessage)}
+      >
+        Send
+      </button>
     </div>
   );
 }
