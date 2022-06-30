cbdf52fe8e4cc982472a79103710507c15528780
ilhamgum <ilham.gumilang29@gmail.com>
2022-06-30 13:05:48 +0700

    add friend from strangers

--- client/src/components/Homepage/Stranger.js
+++ client/src/components/Homepage/Stranger.js
@@ -1,8 +1,49 @@
 import React from "react";
 import { gun, user } from "../../useGun";
+import toast, { Toaster } from "react-hot-toast";
 
 export default function Stranger() {
   const [stranger, setStranger] = React.useState([]);
+
+  const addFriend = React.useCallback(async (input) => {
+    let pub = `~${input}`;
+    let existInUserList; // state given if user existInUserList
+    let existInFriendList;
+
+    // iterate through all list
+    gun
+      .get("userlist")
+      .map()
+      .once((ack) => (ack === pub ? (existInUserList = true) : ""));
+
+    user
+      .get("friends")
+      .map()
+      .once((ack) => (ack === pub ? (existInFriendList = true) : ""));
+
+    // check if user exist in user list
+    if (existInUserList === true) {
+      // user input their pub key itself
+      if (pub === `~${user.is.pub}`) {
+        toast.error("can't add yourself to the list");
+      }
+      // input already in friend list
+      else if (existInFriendList === true) {
+        toast.error("already in your friend list");
+      }
+      // user exist in userlist and can be add to friend list
+      else {
+        user.get("friends").set(pub);
+        toast.success("added as friend, let's holla at them! 💫, page will reload", {
+          duration: 3000,
+        });
+      }
+    }
+    // input not match with any pub in userlist
+    else {
+      toast.error("user not exist");
+    }
+  }, []);
 
   React.useEffect(() => {
     // get incoming message that's not from friends
@@ -27,7 +68,6 @@ export default function Stranger() {
             .map()
             .once(async (e) => {
               friends.push(e.slice(1));
-              console.log(friends);
               gun
                 .get("chat")
                 .map()
@@ -45,7 +85,7 @@ export default function Stranger() {
             });
     };
     getIncomingMessages();
-  }, []);
+  }, [addFriend]);
 
   return (
     <div className="flex flex-col divide-y max-h-full overflow-y-auto overflow-x-hidden scrollbar">
@@ -55,7 +95,8 @@ export default function Stranger() {
             className="hover:bg-blue-100/50 text-white py-2 px-4 rounded focus:outline-none focus:shadow-outline cursor-pointer"
             key={Math.random()}
             onClick={() => {
-              console.log(e);
+              addFriend(e);
+              setTimeout(() => window.location.reload(), 1000)
             }}
           >
             <div className="flex items-center">
@@ -71,6 +112,7 @@ export default function Stranger() {
           </div>
         );
       })}
+      <Toaster />
     </div>
   );
 }
